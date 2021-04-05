f2cbf9417e4abd19ef96a1f253edb961a701e940
@BlueTechno <malavervj@gmail.com>
2021-04-05 15:39:48 -0400

    Agregar archivos de proyecto.

---
+++ Selector de Colores/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace Selector_de_Colores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {                      
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(47, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Button1_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(65, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(164, 145);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(47, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox2_KeyDown);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 78);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(47, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            this.textBox3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox3_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 319);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Selector de Colores/Form1.cs
@@ -0,0 +1,130 @@
+﻿using BlueTechno;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Selector_de_Colores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Selector de Colores";
+            label1.Text = "";
+            button1.Text = "Colorear";
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            MaximizeBox = false;
+        }
+
+        private void TextBoxHandler(TextBox textBox)
+        {
+            try
+            {
+                if (textBox.Text == "")
+                {
+                    return;
+                }
+
+                if (Convert.ToInt32(textBox.Text) > 255)
+                {
+                    textBox.Text = "255";
+                }
+            }
+
+            catch (Exception)
+            {
+                textBox.Clear();
+            }
+        }
+
+        private void EnterPress(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Button1_Click(sender, e);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int A, B, C;
+            string AA, BB, CC;
+
+            try
+            {
+
+                A = Convert.ToInt32(textBox1.Text);
+                B = Convert.ToInt32(textBox2.Text);
+                C = Convert.ToInt32(textBox3.Text);
+
+            }
+
+            catch (Exception)
+            {
+                return;
+            }
+
+            pictureBox1.BackColor = Color.FromArgb(A, B, C);
+
+            AA = Converters.DecimalToHexadecimal(A);
+            BB = Converters.DecimalToHexadecimal(B);
+            CC = Converters.DecimalToHexadecimal(C);
+
+            try
+            {
+                if (Convert.ToInt32(AA) <= 9 || AA == "A" || AA == "B" || AA == "C" || AA == "D" || AA == "E" || AA == "F") { AA = AA.Insert(0, "0"); }
+                if (Convert.ToInt32(BB) <= 9 || BB == "A" || BB == "B" || BB == "C" || BB == "D" || BB == "E" || BB == "F") { BB = BB.Insert(0, "0"); }
+                if (Convert.ToInt32(CC) <= 9 || CC == "A" || CC == "B" || CC == "C" || CC == "D" || CC == "E" || CC == "F") { CC = CC.Insert(0, "0"); }
+            }
+
+            catch (Exception E) { MessageBox.Show(E.ToString()); }
+
+            label1.Text = AA + BB + CC;
+        }
+
+        #region TextChanged
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxHandler(textBox1);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxHandler(textBox2);
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxHandler(textBox3);
+        }
+
+        #endregion
+
+        #region Key Down
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            EnterPress(sender, e);
+        }
+
+        private void TextBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            EnterPress(sender, e);
+        }
+
+        private void TextBox3_KeyDown(object sender, KeyEventArgs e)
+        {
+            EnterPress(sender, e);
+        }
+
+        private void Button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            EnterPress(sender, e);
+        }
+
+        #endregion
+
+    }
+}
